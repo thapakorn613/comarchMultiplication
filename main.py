d46a4da1f89cd4e5de616dc0006be9b1639f9626
thapakorn pheyporn <thapakorn613@gmail.com>
2018-11-17 21:12:18 +0700

    complete for code no intstruction
    คูณได้ถูกต้อง

--- main.py
+++ main.py
@@ -1,56 +1,47 @@
 #!/usr/bin/python
 import toFile as function
-#f1 = 0b010111010
-#f2 = bin(f1 << 1)
-
-mcand = 2
-mplier = 6
+mcand = 10383
+mplier = 32766
 x1 = 0
 result = int(mcand*mplier)
-print ("result[x1] = " , x1 )
+print ("result[x1] = " , result )
 print("-----------------------------------")
 
-indexOfM = 4
-indexOfP = 8
+sizeOfMulti = 15
+sizeOfProduct = 32
 # initial value
-x2 = function.numToBinary(mcand,4)
-x3 = function.numToBinary(mplier,4)
-result = function.numToBinary(result,8)
-x1 = function.numToBinary(mplier,8)
+x2 = function.numToBinary(mcand,sizeOfMulti) # mcand
+x3 = function.numToBinary(mplier,sizeOfMulti) # mplier
+result = function.numToBinary(result,sizeOfProduct) #
+x1 = function.numToBinary(mplier,sizeOfProduct) # Product
 print ("mcand  : " , x2) # x2 is mcand in bin
 print ("mplier : " , x3) # x3 is mplier in bin
 print ("result[Multi] : " , result)
-print ("x1 : " , x1) # x1 is Product in bin
+print ("product : " , x1) # x1 is Product in bin
 #
 product = list(x1) # for check
-temp = function.numToBinary((int(x2,2)+ int(x3,2)),4) # add binary
+temp = function.numToBinary((int(x2,2)+ int(x3,2)),sizeOfMulti) # add binary
 print ("temp: ",temp)
-counter = 0;
+x2 = x2 + function.numToBinary(0,sizeOfMulti)
+zero = function.numToBinary(0,sizeOfProduct)
 print ("--------------------------------")
-for x in range(1):
+for x in range ( sizeOfMulti ):
     print("state[", x+1, "] : ")
-
-    if(product[ indexOfP - 1 ] == '1'):
-        print("1 ")
+    if(product[ sizeOfProduct - 1 ] == '1'):
+        print("1 -> PI = PI + M ")
         print("product : ", x1)
-        x2 = x2 + "0000"
-        x1 = function.numToBinary((int(x1, 2) + int(x2, 2)), 8)
-    elif (product[ indexOfP - 1 ] == '0'):
-        print ("0")
+        x1 = function.numToBinary((int(x1, 2) + int(x2, 2)), sizeOfProduct)
+    elif (product[ sizeOfProduct - 1 ] == '0'):
+        print ("0 -> no op")
         print("product : ", x1)
-        zero = "00000000"
-        tempResult = int(x1, 2) + int(zero, 2)
-        x1 = function.numToBinary(tempResult, 8)
+        x1 = function.numToBinary(int(x1, 2) + int(zero, 2), sizeOfProduct)
     # ----- shift right
     print("Mcand : ", x2)
-    print("Product  : ", x1)
-    # how to convert x1 to binary on form 0bxxxxxxx
-    x1 = bin(int(x1) >> 1)
-    #
-    print(x1)
+    # shift right 1 bit and store to x1
+    x1 = bin(int(x1,base = 2) >> 1) # convert x1 to binary on form 0bxxxxxxx
     tempx1 = x1.split('b')
-    x1 = function.numToBinary(int(tempx1[1],2),8)
-    print ("product : ",x1)
+    x1 = function.numToBinary(int(tempx1[1],2),sizeOfProduct)
+    print("Product  : ", x1)
     product = list(x1)
     print("--------------------------------")
 
